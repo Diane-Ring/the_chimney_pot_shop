d0c14a496f6df6cf69785680e3a00d98fd3f3f59
Diane-Ring <diane-hawkins@hotmail.co.uk>
2026-01-14 14:03:05 +0000

    Add url for product_detail to index.html.  build a product post view

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 #]
 
 urlpatterns = [
-    path("",views.ProductList.as_view(),name='home')
+    path("",views.ProductList.as_view(),name='home'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Product
 
@@ -13,3 +13,26 @@ class ProductList(generic.ListView):
     queryset = Product.objects.filter(status=1).order_by("-created_on")
     template_name = "products/index.html"
     paginate_by = 6
+
+def product_detail(request, slug):
+    """
+    Display an individual :model:`products.Product`.
+
+    **Context**
+
+    ``product``
+        An instance of :model:`products.Product`.
+
+    **Template:**
+
+    :template:`products/product_detail.html`
+    """
+
+    queryset = Product.objects.filter(status=1)
+    product = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "products/product_detail.html",
+        {"product": product},
+    )
