c2a73de36cbfc80986eab19d60d9121fa639cb74
Diane-Ring <diane-hawkins@hotmail.co.uk>
2026-01-15 13:10:38 +0000

    Create edit and delete review function,

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Product
+from django.http import HttpResponseRedirect
+from .models import Product, Review
 from .forms import ReviewForm
 
 #class HomePage(TemplateView):
@@ -32,8 +33,6 @@ def product_detail(request, slug):
 
     queryset = Product.objects.filter(status=1)
     product = get_object_or_404(queryset, slug=slug)
-    reviews = product.review.all().order_by("-created_on")
-    review_count = product.review.filter(approved=True).count()
 
     if request.method == "POST":
         review_form = ReviewForm(data=request.POST)
@@ -45,10 +44,13 @@ def product_detail(request, slug):
             messages.add_message(
                 request, messages.SUCCESS,
                 'Review submitted and awaiting approval'
-    )
-
+            )
+            return HttpResponseRedirect(reverse('product_detail', args=[slug]))
     else:
         review_form = ReviewForm()
+
+    reviews = product.review.all().order_by("-created_on")
+    review_count = product.review.filter(approved=True).count()
 
     return render(
         request,
@@ -60,3 +62,43 @@ def product_detail(request, slug):
             "review_form": review_form,
         },
     )
+
+def review_edit(request, slug, review_id):
+    """
+    view to edit reviews
+    """
+    if request.method == "POST":
+
+        queryset = Product.objects.filter(status=1)
+        product = get_object_or_404(queryset, slug=slug)
+        review = get_object_or_404(Review, pk=review_id)
+        review_form = ReviewForm(data=request.POST, instance=review)
+
+        if review_form.is_valid() and review.author == request.user:
+            review = review_form.save(commit=False)
+            review.product = product
+            review.approved = False
+            review.save()
+            messages.add_message(request, messages.SUCCESS, 'Review Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating review!')
+
+    return HttpResponseRedirect(reverse('product_detail', args=[slug]))
+
+def review_delete(request, slug, review_id):
+    """
+    view to delete review
+    """
+    queryset = Product.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    review = get_object_or_404(Review, pk=review_id)
+
+    if review.author == request.user:
+        review.delete()
+        messages.add_message(request, messages.SUCCESS, 'review deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own reviews!')
+
+    return HttpResponseRedirect(reverse('product_detail', args=[slug]))
+
+
