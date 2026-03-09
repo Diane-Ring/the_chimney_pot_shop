074205cbebc352364a8e165287107ed233cb6627
Diane-Ring <diane-hawkins@hotmail.co.uk>
2026-03-09 14:31:02 +0000

    Delete function repaired

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -3,7 +3,7 @@ const reviewText = document.getElementById("id_body");
 const reviewForm = document.getElementById("reviewForm");
 const submitButton = document.getElementById("submitButton");
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteButtons = document.querySelectorAll(".btn-delete[review_id]");
 const deleteConfirm = document.getElementById("deleteConfirm");
 /**
 * Initializes edit functionality for the provided edit buttons.
@@ -38,7 +38,8 @@ for (let button of editButtons) {
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         let reviewId = e.target.getAttribute("review_id");
-        deleteConfirm.href = `delete_review/${reviewId}`;
+        const basePath = window.location.pathname.replace(/\/$/, "");
+        deleteConfirm.href = `${basePath}/delete_review/${reviewId}`;
         deleteModal.show();
     });
 }
